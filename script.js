69b4888d482ba1cc149fc0dfd65ddf7b80c383e8
VitorHmaia <vitorhmaia@outlook.com>
2023-05-30 17:13:57 -0300

    mdbootstrap added and intro added

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-/* Botão responsividade */
 
-var toggle_Bt=document.querySelector(".togglebtn");
-var nav=document.querySelector(".navlinks");
-var links=document.querySelector(".navlinks li");
-
-toggle_Bt.addEventListener("click",function(){
-    this.classList.toggle("click");
-    nav.classList.toggle("open");
-})
-
-var typed = new Typed(".input", {
-    strings:["Este é o meu portifólio", "Seja bem-vindo!"],
-    typeSpeed:80,
-    backSpeed:45,
+var typed = new Typed("#input", {
+    strings:["Este é o meu portifólio", "Seja bem-vindo!", "Meu nome é Vítor"],
+    typeSpeed:70,
+    backSpeed:30,
     loop:true
 })
+
